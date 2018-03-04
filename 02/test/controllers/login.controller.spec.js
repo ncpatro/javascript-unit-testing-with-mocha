8e2cb2da5130cc5d568c1c722232f3f3c516d9e7
patro <Nrusingha.Patro@target.com>
2018-03-04 21:52:05 +0530

    formating

--- 02/test/controllers/login.controller.spec.js
+++ 02/test/controllers/login.controller.spec.js
@@ -2,14 +2,19 @@ var assert = require('assert');
 var loginController =  require('../../controllers/login.controller');
 
 describe('LoginController', function () {
+
   describe('isValidUserId', function(){
+
     it('should return true if valid user id', function(){
       var isValid = loginController.isValidUserId(['abc123','xyz321'], 'abc123')
       assert.equal(isValid, true);
     });
+
     it('should return false if invalid user id', function(){
       var isValid = loginController.isValidUserId(['abc123','xyz321'],'abc1234')
       assert.equal(isValid, false);
     });
+
   });
+
 });
