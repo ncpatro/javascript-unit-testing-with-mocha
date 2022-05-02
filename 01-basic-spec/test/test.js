f08ed018829f9dd237ac678c1105f21bb104acf2
Tim Zenner <epidemicz@gmail.com>
2022-05-02 08:48:58 -0500

    Update test.js
    fixed typo

--- 01-basic-spec/test/test.js
+++ 01-basic-spec/test/test.js
@@ -1,9 +1,9 @@
 var assert = require('assert');
 describe('Basic Mocha String Test', function () {
- it('should return number of charachters in a string', function () {
+ it('should return number of characters in a string', function () {
         assert.equal("Hello".length, 5);
     });
- it('should return first charachter of the string', function () {
+ it('should return first character of the string', function () {
         assert.equal("Hello".charAt(0), 'H');
         //throw {myError:'throwing error to fail test'}
     });
